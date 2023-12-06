f3a64cb346cfb8f0b190b9617097c56fb75b761f
PJ <pj>
2023-12-06 16:58:12 +0100

    update the index format to AI search GA

--- confluence_vector_sync/config.py
+++ confluence_vector_sync/config.py
@@ -12,7 +12,7 @@ def get_config():
         "azure_search_key": os.getenv("AZURE_SEARCH_KEY"),
         "azure_search_full_reindex": os.getenv("AZURE_SEARCH_FULL_REINDEX", "false").lower() == "true",
         "azure_search_embedding_model": os.getenv("AZURE_SEARCH_EMBEDDING_MODEL", "text-embedding-ada-002"),
-        "azure_search_api_version": os.getenv("AZURE_SEARCH_API_VERSION", "2023-07-01-Preview"),
+        "azure_search_api_version": os.getenv("AZURE_SEARCH_API_VERSION", "2023-11-01"),
         "azure_search_confluence_index": os.getenv("AZURE_SEARCH_CONFLUENCE_INDEX", "confluence"),
         "confluence_url": os.getenv("CONFLUENCE_URL"),
         "confluence_user_name": os.getenv("CONFLUENCE_USER_NAME"),
